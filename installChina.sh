9b9de5e383d23a66b9aa5685a4d34725bd23d9c1
sunliang <sunliang711@163.com>
2021-01-28 13:51:38 +0800

    remove BASH_SOURCE,when curl .. | bash ,BASH_SOURCE is empty

--- installChina.sh
+++ installChina.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-rpath="$(readlink ${BASH_SOURCE})"
-if [ -z "$rpath" ];then
-    rpath=${BASH_SOURCE}
-fi
-pwd=${PWD}
-this="$(cd $(dirname $rpath) && pwd)"
-# cd "$this"
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 user="${SUDO_USER:-$(whoami)}"
@@ -35,6 +28,7 @@ else
     BOLD=""
     NORMAL=""
 fi
+
 _err(){
     echo "$*" >&2
 }
@@ -67,21 +61,10 @@ function _root(){
     fi
 }
 
-ed=vi
-if command -v vim >/dev/null 2>&1;then
-    ed=vim
-fi
-if command -v nvim >/dev/null 2>&1;then
-    ed=nvim
-fi
-if [ -n "${editor}" ];then
-    ed=${editor}
-fi
 ###############################################################################
 # write your code below (just define function[s])
 # function is hidden when begin with '_'
 ###############################################################################
-# TODO
 install(){
     local dest="$home/.app"
     if [ ! -e "${dest}" ];then
@@ -97,61 +80,6 @@ install(){
     cd init2
     ./gitShellTmux.sh
 }
-
-em(){
-    $ed $0
-}
-
 ###############################################################################
-# write your code above
-###############################################################################
-function _help(){
-    cd "${this}"
-    cat<<EOF2
-Usage: $(basename $0) ${bold}CMD${reset}
 
-${bold}CMD${reset}:
-EOF2
-    # perl -lne 'print "\t$1" if /^\s*(\w+)\(\)\{$/' $(basename ${BASH_SOURCE})
-    # perl -lne 'print "\t$2" if /^\s*(function)?\s*(\w+)\(\)\{$/' $(basename ${BASH_SOURCE}) | grep -v '^\t_'
-    perl -lne 'print "\t$2" if /^\s*(function)?\s*(\w+)\(\)\{$/' $(basename ${BASH_SOURCE}) | perl -lne "print if /^\t[^_]/"
-}
-
-function _loadENV(){
-    if [ -z "$INIT_HTTP_PROXY" ];then
-        echo "INIT_HTTP_PROXY is empty"
-        echo -n "Enter http proxy: (if you need) "
-        read INIT_HTTP_PROXY
-    fi
-    if [ -n "$INIT_HTTP_PROXY" ];then
-        echo "set http proxy to $INIT_HTTP_PROXY"
-        export http_proxy=$INIT_HTTP_PROXY
-        export https_proxy=$INIT_HTTP_PROXY
-        export HTTP_PROXY=$INIT_HTTP_PROXY
-        export HTTPS_PROXY=$INIT_HTTP_PROXY
-        git config --global http.proxy $INIT_HTTP_PROXY
-        git config --global https.proxy $INIT_HTTP_PROXY
-    else
-        echo "No use http proxy"
-    fi
-}
-
-function _unloadENV(){
-    if [ -n "$https_proxy" ];then
-        unset http_proxy
-        unset https_proxy
-        unset HTTP_PROXY
-        unset HTTPS_PROXY
-        git config --global --unset-all http.proxy
-        git config --global --unset-all https.proxy
-    fi
-}
-
-
-case "$1" in
-     ""|-h|--help|help)
-        _help
-        ;;
-    *)
-        "$@"
-esac
+install
