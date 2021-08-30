69b42fc5dc672f616f6f41cc17085acf453fefe6
sunliang711 <sunliang711@163.com>
2021-08-30 17:26:14 +0800

    alternative node exporter download link

--- tools/grafana/install.sh
+++ tools/grafana/install.sh
@@ -442,7 +442,12 @@ inodeexporter(){
 
     _infoln "downlad node exporter.."
     version=1.0.1
-    wget -P /tmp https://github.com/prometheus/node_exporter/releases/download/v${version}/node_exporter-${version}.linux-amd64.tar.gz
+    nodeExporterLink="https://github.com/prometheus/node_exporter/releases/download/v${version}/node_exporter-${version}.linux-amd64.tar.gz"
+    wget -P /tmp --timeout=20 "${nodeExporterLink}"
+    if [ $? -ne 0 ];then
+        nodeExporterLink="https://source711.oss-cn-shanghai.aliyuncs.com/nodeExporter/node_exporter-1.0.1.linux-amd64.tar.gz"
+        wget -P /tmp --timeout=20 "${nodeExporterLink}" || { echo "download node exporter failed."; exit 1; }
+    fi
     tar -xzvf /tmp/node_exporter-${version}.linux-amd64.tar.gz -C /tmp
     mv /tmp/node_exporter-${version}.linux-amd64/node_exporter /usr/local/bin/
 
