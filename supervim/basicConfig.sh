a31fab1cdb07a2a71dca2a0a09aa79982040747f
sunliang <sunliang711@163.com>
2021-12-10 10:40:49 +0800

    switchSolanaRpc;vimrc basic install script;

---
+++ supervim/basicConfig.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+if [ -z "${BASH_SOURCE}" ]; then
+    this=${PWD}
+else
+    rpath="$(readlink ${BASH_SOURCE})"
+    if [ -z "$rpath" ]; then
+        rpath=${BASH_SOURCE}
+    elif echo "$rpath" | grep -q '^/'; then
+        # absolute path
+        echo
+    else
+        # relative path
+        rpath="$(dirname ${BASH_SOURCE})/$rpath"
+    fi
+    this="$(cd $(dirname $rpath) && pwd)"
+fi
+
+if [ -r ${SHELLRC_ROOT}/shellrc.d/shelllib ];then
+    source ${SHELLRC_ROOT}/shellrc.d/shelllib
+elif [ -r /tmp/shelllib ];then
+    source /tmp/shelllib
+else
+    # download shelllib then source
+    shelllibURL=https://gitee.com/sunliang711/init2/raw/master/shell/shellrc.d/shelllib
+    (cd /tmp && curl -s -LO ${shelllibURL})
+    if [ -r /tmp/shelllib ];then
+        source /tmp/shelllib
+    fi
+fi
+
+
+###############################################################################
+# write your code below (just define function[s])
+# function is hidden when begin with '_'
+
+globalVimrcPath=/usr/share/vim
+userVimrcPath=$home
+
+vimGlobal(){
+    _require_root
+
+    if [ ! -d ${globalVimrcPath} ];then
+        mkdir -pv ${globalVimrcPath}
+    fi
+
+    if [ -e ${globalVimrcPath}/vimrc ];then
+        echo "already exists ${globalVimrcPath}/vimrc, skip.."
+        return 0
+    fi
+
+    echo "install vimrc to ${globalVimrcPath}.."
+    cp ${this}/basic-pre.vim ${globalVimrcPath}/vimrc
+}
+
+vimUser(){
+    if [ -e ${userVimrcPath}/.vimrc ];then
+        echo "already exists ${userVimrcPath}/.vimrc, skip.."
+        return 0
+    fi
+
+    echo "install vimrc to ${userVimrcPath}.."
+    cp ${this}/basic-pre.vim ${userVimrcPath}/.vimrc
+}
+
+userNvimrcPath=$home/.config/nvim
+nvim(){
+
+    if [ ! -d ${userNvimrcPath} ];then
+        mkdir -pv ${userNvimrcPath}
+    fi
+
+    if [ -e ${userNvimrcPath}/init.vim ];then
+        echo "already exists ${userNvimrcPath}/init.vim skip.."
+        return 0
+    fi
+    echo "install vimrc to ${userNvimrcPath}/init.vim.."
+    cp ${this}/basic-pre.vim ${userNvimrcPath}/init.vim
+
+}
+
+# write your code above
+###############################################################################
+
+em(){
+    $ed $0
+}
+
+function _help(){
+    cd "${this}"
+    cat<<EOF2
+Usage: $(basename $0) ${bold}CMD${reset}
+
+${bold}CMD${reset}:
+EOF2
+    perl -lne 'print "\t$2" if /^\s*(function)?\s*(\S+)\s*\(\)\s*\{$/' $(basename ${BASH_SOURCE}) | perl -lne "print if /^\t[^_]/"
+}
+
+case "$1" in
+     ""|-h|--help|help)
+        _help
+        ;;
+    *)
+        "$@"
+esac
