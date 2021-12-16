8145a8a93ff591389706aa6c2bc858da812ab4dd
sunliang <sunliang711@163.com>
2021-12-16 14:04:21 +0800

    wireguard use soft link;

--- tools/wireguard/install.sh
+++ tools/wireguard/install.sh
@@ -52,9 +52,22 @@ install(){
 
     echo -n "Enter server port: "
     read serverPort
-    # install wireguard.sh
-    sed -e "s|<SERVER_PORT>|${serverPort}|g" wireguard.sh >/tmp/wireguard.sh && chmod +x /tmp/wireguard.sh
-    mv /tmp/wireguard.sh /usr/local/bin
+    # # install wireguard.sh
+    # sed -e "s|<SERVER_PORT>|${serverPort}|g" wireguard.sh >/tmp/wireguard.sh && chmod +x /tmp/wireguard.sh
+    # mv /tmp/wireguard.sh /usr/local/bin
+    ln -sf ${this}/wireguard.sh /usr/local/bin
+
+    cat<<-EOF>${wireguardRoot}/settings
+	serverPubkey=server-publickey
+	serverPrikey=server-privatekey
+	serverConfigName=wg0
+	serverConfigFile=${serverConfigName}.conf
+	MTU=1420
+	subnet=10.10.10
+	serverIp=${subnet}.1/24
+	serverPort=${serverPort}
+	tableNo=10
+	EOF
 
     # config server
     /usr/local/bin/wireguard.sh configServer
