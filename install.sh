e78309b8342133fc51ed91831269a4c16e0e467b
sunliang <sunliang711@163.com>
2022-06-25 20:48:37 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+user="${SUDO_USER:-$(whoami)}"
+home="$(eval echo ~$user)"
+
+# export TERM=xterm-256color
+
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+  ncolors=$(tput colors 2>/dev/null)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    CYAN="$(tput setaf 5)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    CYAN=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+fi
+
+_err(){
+    echo "$*" >&2
+}
+
+_runAsRoot(){
+    cmd="${*}"
+    local rootID=0
+    if [ "${EUID}" -ne "${rootID}" ];then
+        echo -n "Not root, try to run '${cmd}' as root.."
+        # or sudo sh -c ${cmd} ?
+        if eval "sudo ${cmd}";then
+            echo "ok"
+            return 0
+        else
+            echo "failed"
+            return 1
+        fi
+    else
+        # or sh -c ${cmd} ?
+        eval "${cmd}"
+    fi
+}
+
+rootID=0
+function _root(){
+    if [ ${EUID} -ne ${rootID} ];then
+        echo "Need run as root!"
+        echo "Requires root privileges."
+        exit 1
+    fi
+}
+
+###############################################################################
+# write your code below (just define function[s])
+# function is hidden when begin with '_'
+###############################################################################
+install(){
+    local dest="$home/.local/apps"
+    if [ ! -e "${dest}" ];then
+        mkdir -p "${dest}"
+    fi
+    if [ -e ${dest}/init2 ];then
+        echo "Already exists ${dest}/init2"
+        exit
+    fi
+
+    cd "${dest}"
+    git clone https://github.com/sunliang711/init2
+    cd init2
+    ./gitShellTmux.sh -o
+}
+###############################################################################
+
+install
+
