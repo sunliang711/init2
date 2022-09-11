6845ad942d1317a833cc40f079c8b9e05df6d565
sunliang711 <sunliang711@163.com>
2022-09-11 12:10:55 +0800

    zsh install newts

---
+++ shell/installZsh.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+if [ -z "${BASH_SOURCE}" ]; then
+    this=${PWD}
+else
+    rpath="$(readlink ${BASH_SOURCE})"
+    if [ -z "$rpath" ]; then
+        rpath=${BASH_SOURCE}
+    elif echo "$rpath" | grep -q '^/'; then
+        # absolute path
+        echo
+    else
+        # relative path
+        rpath="$(dirname ${BASH_SOURCE})/$rpath"
+    fi
+    this="$(cd $(dirname $rpath) && pwd)"
+fi
+
+if [ -r ${SHELLRC_ROOT}/shellrc.d/shelllib ];then
+    source ${SHELLRC_ROOT}/shellrc.d/shelllib
+elif [ -r /tmp/shelllib ];then
+    source /tmp/shelllib
+else
+    # download shelllib then source
+    shelllibURL=https://gitee.com/sunliang711/init2/raw/master/shell/shellrc.d/shelllib
+    (cd /tmp && curl -s -LO ${shelllibURL})
+    if [ -r /tmp/shelllib ];then
+        source /tmp/shelllib
+    fi
+fi
+
+
+###############################################################################
+# write your code below (just define function[s])
+# function is hidden when begin with '_'
+step1(){
+    _require_command curl
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+step2(){
+    _require_command git
+    set -xe
+
+    # zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+    # custom theme
+    ln -sf ${PWD}/*.zsh-theme ~/.oh-my-zsh/custom/themes
+
+    sed -ibak \
+        -e 's/\(ZSH_THEME=\).\{1,\}/\1"zeta"/' \
+        -e 's/plugins=.*/plugins=(git cp themes timer golang rust npm yarn zsh-autosuggestions)/' \
+        ~/.zshrc
+    rm ~/.zshrcbak
+    echo "bindkey ',' autosuggest-accept" >> ~/.zshrc
+
+    # zsh-syntax-highlighting
+    [ ! -d ~/.zsh-syntax-highlighting ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
+    # TODO check existence
+    echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
+    startLine="##CUSTOM BEGIN v3"
+    endLine="##CUSTOM END v3"
+    configFile=~/.zshrc
+    export SHELLRC_ROOT=${this}
+    if ! grep -q "$startLine" "${configFile}";then
+        cat <<-EOF >> "$configFile"
+	$startLine
+	export SHELLRC_ROOT=${this}
+	source \${SHELLRC_ROOT}/shellrc
+	$endLine
+	EOF
+    fi
+
+    if [ ! -e "$HOME/.editrc" ] || ! grep -q 'bind -v' "$HOME/.editrc";then
+        echo 'bind -v' >> "$HOME/.editrc"
+    fi
+    if [ ! -e "$HOME"/.inputrc ] || ! grep -q 'set editing-mode vi' "$HOME/.inputrc";then
+        echo 'set editing-mode vi' >> "$HOME/.inputrc"
+    fi
+
+    # soft link sshconfig
+    [ ! -d ~/.ssh ] && mkdir ~/.ssh
+    ln -sf ${SHELLRC_ROOT}/shellrc.d/sshconfig $HOME/.ssh/config
+}
+
+uninstall(){
+    echo
+}
+
+# write your code above
+###############################################################################
+
+em(){
+    $ed $0
+}
+
+function _help(){
+    cd "${this}"
+    cat<<EOF2
+Usage: $(basename $0) ${bold}CMD${reset}
+
+${bold}CMD${reset}:
+EOF2
+    perl -lne 'print "\t$2" if /^\s*(function)?\s*(\S+)\s*\(\)\s*\{$/' $(basename ${BASH_SOURCE}) | perl -lne "print if /^\t[^_]/"
+}
+
+case "$1" in
+     ""|-h|--help|help)
+        _help
+        ;;
+    *)
+        "$@"
+esac
